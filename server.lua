e1fc0ec44db2aebf6aaf58e35d93b97a82986e3a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-02-03 11:01:34 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 
 local function RequestResourceVersionByUrl(url, resource)
-    PerformHttpRequest('https://raw.githubusercontent.com/TPZ-CORE/' .. url .. '/main/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/TPZ-CORE/' .. url .. '/main/fxmanifest.lua', function(err, text, headers)
         local currentVersion = GetResourceMetadata(resource, 'version')
 
         if not text then 
